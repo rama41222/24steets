a6b7af179b80e5913381da82d497dcfb1ee5cf7d
Dinushanka Nayomal <Rama41222@Dinushankas-MacBook-Pro.local>
2017-07-13 00:43:47 +0530

    code refinements

--- app/controllers/intro.js
+++ app/controllers/intro.js
@@ -5,9 +5,7 @@ var countDownDate = new Date("Jan 1, 2018 00:00:00").getTime();
 var x = setInterval(function() {
 
   var now = new Date().getTime();
-
   var distance = countDownDate - now;
-
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
